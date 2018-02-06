922a85dace3b59087e3740b5043c9e3758c902e9
Ahnaf Hasan <ahasan1@stuy.edu>
2018-02-05 20:56:37 -0500

    Updated APCS to include all files

--- Java Files/NewFile.java
+++
@@ -1,8 +0,0 @@
-public class NewFile {
-    public static void main(String [] args) {
-	BankAccount john = new BankAccount();
-	john.setAcctNum(123456789);
-	john.setPasswd("pass123");
-	john.ThisU(123456789, "pass123");
-    }
-}
